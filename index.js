4a6421c70b57c4f090f5265a172b3a4190195bd0
Henri <henri-joel.mugu@voco.ee>
2022-12-13 11:34:48 +0200

    add static files - css and js for styling

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const path = require("path")
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 
+//static files - css, js, images
+app.use(express.static(path.join(__dirname, "public")))
+
 app.get('/', (req, res)=>{
     res.render('index')
 })
