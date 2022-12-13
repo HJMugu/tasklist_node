2285a7ec4a7750bb21923337b0d841cd90fdbcc2
Henri <henri-joel.mugu@voco.ee>
2022-12-13 11:22:31 +0200

    added main view

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const app = express()
 
+//add template engine
+const path = require("path")
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
+
 app.get('/', (req, res)=>{
-    res.send("hello world")
+    res.render('index')
 })
 
 app.listen(3002)
